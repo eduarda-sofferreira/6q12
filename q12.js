8f2f4c4a42c693f2ed56f3513a78b5ca91ad87e3
eduarda-sofferreira <espf1@aluno.ifal.edu.br>
2024-12-09 19:45:42 -0300

    Create q12.js

---
+++ q12.js
@@ -0,0 +1,11 @@
+var c = 1
+var pares = 0
+while ( c <= 20) {
+    var numero = parseInt (prompt ("informe um número"))
+    if ( numero % == 0){
+        pares++;
+    }
+    c++
+}
+
+    console.log(pares + "são números pares")
